7122be95a9ee14f86ece4a95f036ebecf04d3808
Sushant-sushii <kumar1sushantsingh.com>
2026-07-01 14:29:29 +0530

    removed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Scene from "./components/Scene"
 import Strings from "./components/Strings"
 import IntroCards from "./components/IntroCards"
 import Footer from "./components/Footer"
-import { Analytics } from "@vercel/analytics/next"
-import { useParams, usePathname, useSearchParams } from "next/navigation.js"
+
 // 
 
 
