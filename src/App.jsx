0adcfbecdf220bfd40870909a3caa197a01e8461
Sushant-sushii <kumar1sushantsingh.com>
2026-07-01 14:28:24 +0530

    import alterations

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Strings from "./components/Strings"
 import IntroCards from "./components/IntroCards"
 import Footer from "./components/Footer"
 import { Analytics } from "@vercel/analytics/next"
-
+import { useParams, usePathname, useSearchParams } from "next/navigation.js"
 // 
 
 
